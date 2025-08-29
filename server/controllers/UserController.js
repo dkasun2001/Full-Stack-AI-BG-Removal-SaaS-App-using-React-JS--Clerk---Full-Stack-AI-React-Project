917ec52073afd9a2349381999ff4222a01fbc3e8
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2025-08-29 13:40:28 +0530

    Implement Clerk webhook handling in UserController and add webhook secret to .env

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,38 @@
+import { Webhook, webhook } from "svix";
 // API Controller Funtion for Managing Clerk User with Database
 
 //https://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async (req, res) => {
-  
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // Verify the webhook signature
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+    const { data, type } = req.body; // Destructure data and type from req.body
+
+    switch (type) {
+      case "user.created": {
+        // Handle user created event
+        break;
+      }
+      case "user.updated": {
+        // Handle user updated event
+        break;
+      }
+      case "user.deleted": {
+        // Handle user deleted event
+        break; 
+      }
+      default:
+        console.log("Unhandled event type:", type);
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
